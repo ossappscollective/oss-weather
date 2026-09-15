7cf989689954d68a2a3675c09b57201804a7f27b
Martin Guillon <dev@akylas.fr>
2026-09-15 16:17:47 +0200

    chore: use napi android

--- nativescript.config.js
+++ nativescript.config.js
@@ -31,7 +31,7 @@ module.exports = {
     },
     android: {
         ignoredNativeDependencies: ['@akylas/nativescript-inapp-purchase'],
-        runtimePackageName: '@akylas/nativescript-android-runtime',
+        runtimePackageName: '@nativescript/android-quickjs-ng',
         gradleVersion: '8.14.3',
         markingMode: 'none',
         codeCache: true,
